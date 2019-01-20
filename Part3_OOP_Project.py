77bd9339a8d13fc53b479778f23ac865d65aac24
Lu lu <lulu9091@gmail.com>
2019-01-20 12:31:36 -0600

    Push the Init python file

---
+++ Part3_OOP_Project.py
@@ -0,0 +1,68 @@
+# For this project you will be using OOP to create a card game. This card game will 
+# be the card game "War" for two players, you and the computer. If you don't know 
+# how to play "War" here are the basic rules:
+#
+# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
+# face down. Anyone may deal first. Each player places his stack of cards face down,
+# in front of him.
+#
+# The Play:
+#
+# Each player turns up a card at the same time and the player with the higher card takes
+# both cards and puts them, face down, on the bottom of his stack
+#
+# If the cards are the same rank, it is War. Each player turns up three cards face down 
+# and one card face up. The player with the higher cards takes both piles (six cards). If the 
+# turned-up cards are again the same rank, each player places another card face down and turns
+# another card face up. The player with the higher card takes all 10 cards, and so on.
+#
+# There are some more variations on this but we will keep it simple for now.
+# Ignore "double" wars
+#
+#https://en.wikipedia.org/wiki/War_(card_game)
+
+from random import shuffle
+
+# Two useful variables for creating Cards
+SUITES = 'H D S C'.split()
+RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+class Deck:
+    """
+    THis is the Deck Class. THis object will create a deck of cards to initiate play.
+    You can then use this Deck list of cards to split in half and give to the players. 
+    It will use SUITE and RANKS to create the deck. It should also have a method for 
+    splitting/cutting the deck in half and Shuffling the deck.
+    """
+    def __init__(self):
+        self.deck = ['suite'+'rank' for suite in SUITES for rank in RANKS]
+    def cutting(self):
+        pass
+
+    def shuffling(self):
+        random.shuffle(self.deck)
+        pass
+
+
+    pass
+class Hand:
+    """
+    This is the Hand class. Each player has a Hand, and can add or remove cards from that hand.
+    There should be an add and remove card method here.
+    """
+    pass
+
+class Player:
+    """
+    This is the Player class, which takes in a name and an instance of a Hand class object.
+    The Player can then play can then play cards and check if they still have cards.
+    """
+    pass
+
+
+
+
+
+###########
+#GAME PLAY#
+###########
